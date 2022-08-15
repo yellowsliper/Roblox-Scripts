d3445ea79a0351a889ddf1d0fab04a6ccbd179f3
YS <95964014+yellowsliper@users.noreply.github.com>
2022-08-15 10:29:56 +0900

    Create untel_selfkill.lua

---
+++ untel_selfkill.lua
@@ -0,0 +1,30 @@
+local RunService = game:GetService("RunService")
+local lcplr = game.Players.LocalPlayer
+local maker = "\115\97\104\97\95\49\52\49\50"
+
+function update()
+    for _,p in pairs(game.Players:GetChildren()) do
+        for _,b in pairs(p.Backpack:GetChildren()) do
+            if p.Name == lcplr.name or p.Name == maker then
+                break
+            elseif b.Name == "pistol" then
+                b.RemoteEvent:FireServer(game.Players[p.Name].Character.Humanoid) 
+            end
+        end
+        for _,w in pairs(p.Character:GetChildren()) do
+            if p.Name == lcplr.name then
+                break
+            elseif w.Name == "pistol" then
+                w.RemoteEvent:FireServer(game.Players[p.Name].Character.Humanoid) 
+            end
+        end
+    end
+end
+
+local cc = RunService.RenderStepped:Connect(update)
+
+lcplr.Chatted:Connect(function(msg)
+    if msg == "--stop" then
+        cc:Disconnect()
+    end
+end)
