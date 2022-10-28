deceae722355306ddc9ae42120e6a87d99a6f001
YS <95964014+yellowsliper@users.noreply.github.com>
2022-10-28 19:37:02 +0900

    Update Untel_Hub_Lib.lua

--- Untel_Hub_Lib.lua
+++ Untel_Hub_Lib.lua
@@ -1571,12 +1571,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 	return Window
 end
 
-
-
-function RayfieldLibrary:Destroy()
-	Rayfield:Destroy()
-end
-
 Topbar.ChangeSize.MouseButton1Click:Connect(function()
 	if Debounce then return end
 	if Minimised then
